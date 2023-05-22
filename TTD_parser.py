c0a8adbe559d040fb8f4548125c4f058596d5596
Lucy Zhang <bazhang@scripps.edu>
2023-05-22 11:28:20 -0700

    Merged duplicates for load_target_dis_data Removed duplicates from load_biomarker_dis_data by adding TTD assigned biomarker ID

--- TTD_parser.py
+++ TTD_parser.py
@@ -20,8 +20,7 @@ def get_target_info(file_path):
     for line in tabfile_feeder(target_info_file, header=40):
         if line != ["", "", "", "", ""]:
             if line[1].startswith("TARGETID"):
-                target_info = {}
-                target_info["target_id"] = line[2]
+                target_info = {"target_id": line[2]}
             elif "UNIPROID" in line[1]:
                 target_info["uniprot"] = line[2]
             elif "TARGTYPE" in line[1]:
@@ -47,23 +46,32 @@ def load_drug_target(file_path):
 
     drug_target_data = pd.read_excel(drug_targ_file, engine="openpyxl").to_dict(orient="records")
 
+    target_info_d = {d["target_id"]: d for d in get_target_info(file_path)}
+
     for dicts in drug_target_data:
-        target_info = get_target_info(file_path)
         object_node = {"id": dicts["TargetID"], "type": "biolink:Protein"}
         drug_moa = dicts["MOA"]
 
-        subject_node = {"id": dicts["DrugID"], "trial_status": dicts["Highest_status"].lower(), "type": "biolink:Drug"}
+        subject_node = {
+            "id": dicts["DrugID"],
+            "trial_status": dicts["Highest_status"].lower(),
+            "type": "biolink:Drug",
+        }
         if drug_moa != ".":
             subject_node["moa"] = drug_moa.lower()
 
-        for d in target_info:
-            if object_node["id"] == d["target_id"]:
-                object_node.update(d)
+        if object_node["id"] in target_info_d:
+            object_node.update(target_info_d[object_node["id"]])
 
         _id = f"{subject_node['id']}_associated_with_{object_node['id']}"
         association = {"predicate": "biolink:associated_with"}
 
-        output_dict = {"_id": _id, "association": association, "object": object_node, "subject": subject_node}
+        output_dict = {
+            "_id": _id,
+            "association": association,
+            "object": object_node,
+            "subject": subject_node,
+        }
 
         yield output_dict
 
@@ -96,7 +104,11 @@ def load_drug_dis_data(file_path):
 
                 if drug_id and drug_name:
                     _id = f"{drug_id}_{drug_name}_{icd11}"
-                    dict1 = {"_id": _id, "status": trial_status, "disease": disease_name}
+                    dict1 = {
+                        "_id": _id,
+                        "status": trial_status,
+                        "disease": disease_name,
+                    }
                     drug_dis_list.append(dict1)
                 else:
                     print("Both TTDDRUID and DRUGNAME need to be provided.")
@@ -113,9 +125,16 @@ def load_drug_dis_data(file_path):
         icd11 = _id.split("_")[2]
 
         output_dict["_id"] = f"{drug_id}_treats_{icd11}"
-        output_dict["association"] = {"predicate": "biolink:treats", "clinical_trial": trial_list}
+        output_dict["association"] = {
+            "predicate": "biolink:treats",
+            "clinical_trial": trial_list,
+        }
         output_dict["object"] = {"id": icd11, "icd11": icd11}
-        output_dict["subject"] = {"id": drug_id, "name": drug_name, "type": "biolink:Drug"}
+        output_dict["subject"] = {
+            "id": drug_id,
+            "name": drug_name,
+            "type": "biolink:Drug",
+        }
         disease = output_dict["association"]["clinical_trial"][0]["disease"]
         output_dict["object"]["name"] = disease
         output_dict["object"]["type"] = "biolink:Disease"
@@ -133,43 +152,75 @@ def load_target_dis_data(file_path):
     target_dis_file = os.path.join(file_path, "P1-06-Target_disease.txt")
     assert os.path.exists(target_dis_file)
 
-    subject_node = None
+    target_info_d = {d["target_id"]: d for d in get_target_info(file_path)}
+
+    targ_dis_list = []
+
+    targ_id = None
+    targ_name = None
 
     for line in tabfile_feeder(target_dis_file, header=22):
         if line != [""]:
             if line[1] == "TARGETID":
-                subject_node = {"id": line[2], "target_id": line[2], "name": None, "type": "biolink:Protein"}
+                targ_id = line[2]
             elif line[1] == "TARGNAME":
-                subject_node["name"] = line[2]
+                targ_name = line[2]
             elif line[1] == "INDICATI":
                 icd11 = line[3].split(":")[1].split("]")[0].strip()
-                object_node = {
-                    "id": icd11,
-                    "icd11": icd11,
-                    "name": line[3].split("[")[0].strip(),
-                    "type": "biolink:Disease",
-                }
+                disease_name = line[3].split("[")[0].strip()
 
-                if subject_node and object_node:
-                    _id = f"{subject_node['id']}_target_for_{object_node['icd11']}"
-                    association = {"predicate": "biolink:target_for"}
-
-                    target_info = get_target_info(file_path)
-                    for d in target_info:
-                        if d["target_id"] == subject_node["id"]:
-                            subject_node.update(d)
-
-                    output_dict = {
-                        "_id": _id,
-                        "association": association,
-                        "object": object_node,
-                        "subject": subject_node,
+                if targ_id and targ_name:
+                    dict1 = {
+                        "_id": f"{targ_id}_{targ_name}_{icd11}",
+                        "status": line[2].split("\t")[0],
+                        "disease": disease_name,
                     }
-
-                    yield output_dict
-
+                    targ_dis_list.append(dict1)
                 else:
-                    print("Subject and object need to be provided.")
+                    print("Both target id and target name need to be provided.")
+
+    merged_dict = defaultdict(list)
+    for d in targ_dis_list:
+        merged_dict[d["_id"]].append({"status": d["status"], "disease": d["disease"]})
+
+    for _id, trial_list in merged_dict.items():
+        targ_id = _id.split("_")[0]
+        targ_name = _id.split("_")[1]
+        icd11 = _id.split("_")[2]
+
+        association = {
+            "predicate": "biolink:target_for",
+            "clinical_trial": trial_list,
+        }
+        _id = f"{targ_id}_target_for_{icd11}"
+
+        subject_node = {
+            "id": targ_id,
+            "name": targ_name,
+            "type": "biolink:Protein",
+        }
+
+        object_node = {
+            "id": icd11,
+            "icd11": icd11,
+            "name": association["clinical_trial"][0]["disease"],
+            "type": "biolink:Disease",
+        }
+
+        if subject_node["id"] in target_info_d:
+            subject_node.update(target_info_d[subject_node["id"]])
+
+        if subject_node and object_node:
+            output_dict = {
+                "_id": _id,
+                "association": association,
+                "object": object_node,
+                "subject": subject_node,
+            }
+
+            yield output_dict
+        else:
+            print("Subject and object need to be provided.")
 
 
 def cleanup_icds(line, icd_prefix):
@@ -200,11 +251,19 @@ def load_biomarker_dis_data(file_path):
 
     for line in tabfile_feeder(biomarker_file, header=16):
         if line:
-            subject_node = {"id": line[3].split(":")[1].strip(), "name": line[2], "type": "biolink:Disease"}
+            subject_node = {
+                "id": line[3].split(":")[1].strip(),
+                "name": line[2],
+                "type": "biolink:Disease",
+            }
 
             biomarker_name = line[1]
 
-            icd_group = [(line[3], "ICD-11:"), (line[4], "ICD-10:"), (line[5], "ICD-9:")]
+            icd_group = [
+                (line[3], "ICD-11:"),
+                (line[4], "ICD-10:"),
+                (line[5], "ICD-9:"),
+            ]
 
             for icd_line, icd_prefix in icd_group:
                 icd_value = cleanup_icds(icd_line, icd_prefix)
@@ -221,18 +280,23 @@ def load_biomarker_dis_data(file_path):
 
             if pattern:
                 _id_biomarker = pattern.groups()[1]
-                _id = f"{_id_biomarker}_biomarker_for_{disease_name}"
+                _id = f"{line[0]}_{_id_biomarker}_biomarker_for_{disease_name}"
                 object_node["name"] = pattern.groups()[0]
                 object_node["symbol"] = _id_biomarker
 
             else:
                 _id_biomarker = biomarker_name.replace(" ", "_")
-                _id = f"{_id_biomarker}_biomarker_for_{disease_name}"
+                _id = f"{line[0]}_{_id_biomarker}_biomarker_for_{disease_name}"
                 object_node["name"] = _id_biomarker
 
             association = {"predicate": "biolink:biomarker_for"}
 
-            output_dict = {"_id": _id, "association": association, "object": object_node, "subject": new_subject_node}
+            output_dict = {
+                "_id": _id,
+                "association": association,
+                "object": object_node,
+                "subject": new_subject_node,
+            }
 
             yield output_dict
 
@@ -245,6 +309,8 @@ def load_drug_target_act(file_path):
     """
     activity_file = os.path.join(file_path, "P1-09-Target_compound_activity.txt")
     assert os.path.exists(activity_file)
+
+    target_info_d = {d["target_id"]: d for d in get_target_info(file_path)}
 
     subject_node = {}
     object_node = {}
@@ -259,10 +325,8 @@ def load_drug_target_act(file_path):
         object_node["type"] = "biolink:Protein"
 
         if subject_node and object_node:
-            target_info = get_target_info(file_path)
-            for d in target_info:
-                if d["target_id"] == object_node["id"]:
-                    object_node.update(d)
+            if object_node["id"] in target_info_d:
+                object_node.update(target_info_d[object_node["id"]])
 
             _id = f"{subject_node['id']}_associated_with_{object_node['id']}"
             association = {"predicate": "biolink:associated_with"}
@@ -273,7 +337,12 @@ def load_drug_target_act(file_path):
             else:
                 print(f"{line[3]} pattern does not match.")
 
-            output_dict = {"_id": _id, "association": association, "object": object_node, "subject": subject_node}
+            output_dict = {
+                "_id": _id,
+                "association": association,
+                "object": object_node,
+                "subject": subject_node,
+            }
 
             yield output_dict
 
